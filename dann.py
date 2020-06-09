7f0f14e59be7e7a7adbcbae39c9276e7d7bd6eb5
MatteoT9890 <53305467+MatteoT9890@users.noreply.github.com>
2020-06-09 18:22:40 +0200

    Create dann.py

---
+++ dann.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
+
+
+__all__ = ['AlexNet', 'alexnet']
+
+
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
+
+class ReverseLayerF(Function):
+    # Forwards identity
+    # Sends backward reversed gradients
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
+
+
+class DannNet(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(DannNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+        self.dann_classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 2),
+        )
+
+    def forward(self, x, alpha = None):
+        features = self.features(x)
+        features = self.avgpool(features)
+        features = torch.flatten(features,1)
+        if alpha is not None:
+            reverse_features = ReverseLayerF.apply(features, alpha)
+            discriminator_output = self.dann_classifier(reverse_features)
+            return discriminator_output
+        else:
+             class_outputs = self.classifier(features)
+             return class_outputs
+
+
+def dann_net(pretrained=False, progress=True, **kwargs):
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    net = DannNet(**kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls['alexnet'],
+                                              progress=progress)
+        net.load_state_dict(state_dict, strict = False)
+        net.dann_classifier[1].weight.data = net.classifier[1].weight.data.clone()
+        net.dann_classifier[1].bias.data = net.classifier[1].bias.data.clone()
+        net.dann_classifier[4].weight.data = net.classifier[4].weight.data.clone()
+        net.dann_classifier[4].bias.data = net.classifier[4].bias.data.clone()
+        
+    return net
